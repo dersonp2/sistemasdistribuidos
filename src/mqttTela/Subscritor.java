0737010b41d7ac7962aaa9bbc612a69d931783f8
Anderson <anderson@lsdi.ufma.br>
2020-05-01 12:19:34 -0300

    Alterando o callback do cliente

--- src/mqttTela/Subscritor.java
+++ src/mqttTela/Subscritor.java
@@ -9,7 +9,7 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-public class Subscritor extends javax.swing.JFrame implements MqttCallback {
+public class Subscritor extends javax.swing.JFrame {
 
     MqttClient cliente;
     String brokerUrl = "tcp://localhost:1883";
@@ -24,6 +24,10 @@ public class Subscritor extends javax.swing.JFrame implements MqttCallback {
 
         conectar = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
+        resultado = new javax.swing.JLabel();
+        textoValorTemperatura = new javax.swing.JLabel();
+        textoClima = new javax.swing.JLabel();
+        txtClima = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -36,6 +40,8 @@ public class Subscritor extends javax.swing.JFrame implements MqttCallback {
 
         jLabel1.setText("Subscriber");
 
+        resultado.setText("Valor da temperatura da minha casa:");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -43,12 +49,21 @@ public class Subscritor extends javax.swing.JFrame implements MqttCallback {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
+                        .addGap(180, 180, 180)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
                         .addGap(128, 128, 128)
                         .addComponent(conectar))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(180, 180, 180)
-                        .addComponent(jLabel1)))
-                .addContainerGap(131, Short.MAX_VALUE))
+                        .addGap(43, 43, 43)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(textoValorTemperatura, javax.swing.GroupLayout.PREFERRED_SIZE, 169, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(resultado, javax.swing.GroupLayout.PREFERRED_SIZE, 274, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(textoClima, javax.swing.GroupLayout.PREFERRED_SIZE, 217, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(64, 64, 64)
+                        .addComponent(txtClima, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(83, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -57,22 +72,58 @@ public class Subscritor extends javax.swing.JFrame implements MqttCallback {
                 .addComponent(jLabel1)
                 .addGap(38, 38, 38)
                 .addComponent(conectar)
-                .addContainerGap(214, Short.MAX_VALUE))
+                .addGap(52, 52, 52)
+                .addComponent(resultado)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(textoValorTemperatura, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(txtClima, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(textoClima, javax.swing.GroupLayout.PREFERRED_SIZE, 0, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(70, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void conectarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_conectarActionPerformed
-        Subscritor s = new Subscritor();
+
+        MqttCallback clienteCallBack = new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable thrwbl) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void messageArrived(String string, MqttMessage mm) throws Exception {
+
+                int temperatura = Integer.parseInt(mm.toString());
+
+                String clima = "";
+                if (temperatura < 20) {
+                    clima = "Ta frio";
+                } else if (temperatura >= 20 && temperatura <= 30) {
+                    clima = "Regular";
+                } else if (temperatura > 30) {
+                    clima = "Ta quente";
+                }
+                textoValorTemperatura.setText(mm.toString());
+                txtClima.setText(clima);
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken imdt) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        };
+
         try {
             // instanciar o cliente
             cliente = new MqttClient(brokerUrl, "subscribeClient");
             JOptionPane.showMessageDialog(null, "Conexão realizada com sucesso");
-            cliente.setCallback(s);
+            cliente.setCallback(clienteCallBack);
             cliente.connect();
             cliente.subscribe("/temp", 0);
-            
         } catch (MqttException ex) {
             JOptionPane.showMessageDialog(null, "Erro ao se conectar");
         }
@@ -113,25 +164,10 @@ public class Subscritor extends javax.swing.JFrame implements MqttCallback {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton conectar;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel resultado;
+    private javax.swing.JLabel textoClima;
+    private javax.swing.JLabel textoValorTemperatura;
+    private javax.swing.JLabel txtClima;
     // End of variables declaration//GEN-END:variables
 
-    @Override
-    public void connectionLost(Throwable thrwbl) {
-        JOptionPane.showMessageDialog(null, "Conexão perdida");
-    }
-
-    @Override
-    public void messageArrived(String topico, MqttMessage mm) throws Exception {
-       if(topico.equals("/temp")){
-       String  msg;
-       msg = "Temperatura: "  + mm;
-       
-       JOptionPane.showMessageDialog(null, msg);
-       }
-    }
-
-    @Override
-    public void deliveryComplete(IMqttDeliveryToken imdt) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 }
